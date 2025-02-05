a371af504bf168423fce28758bf46af74899daba
OubelaidAyoub <oubelaid.ayoub2000@gmail.com>
2025-02-05 23:41:37 +0100

    yes

--- src/Components/Images/Pages/Home.jsx
+++ src/Components/Images/Pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
             <header>
                 <nav class="navbar navbar-expand-lg navbar-dark bg-black">
                     <div class="container-fluid">
-                        <a class="navbar-brand" href="#" style={{ fontWeight: "bold" }}>Car-Wow</a>
+                        <a class="navbar-brand" href="#" style={{ fontWeight: "bold" }}>CAR</a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
