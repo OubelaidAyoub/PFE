5d927ce3139f47b8eaf43685add92d6fb23d51bd
OubelaidAyoub <oubelaid.ayoub2000@gmail.com>
2025-02-05 22:44:24 +0100

    New Project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Home from './Components/Images/Pages/Home';
+
+function App() {
+  return (
+    <div class="main">
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Images/Pages/Home.jsx
@@ -0,0 +1,60 @@
+import 'C:/Users/My-Pc/Desktop/bootsrap-project/src/App.css'
+const Home = () => {
+    return (
+        <>  {/* NAVBAR */}
+            <header>
+                <nav class="navbar navbar-expand-lg navbar-dark bg-black">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#" style={{ fontWeight: "bold" }}>Car-Wow</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Nos voitures</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link disabled" aria-disabled="true">Espace garagiste</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Contactez nous
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="#">Instagram : CarWOW25</a></li>
+                                        <li><a class="dropdown-item" href="#">Facebook : CarWOW25</a></li>
+                                        <li><a class="dropdown-item" href="#">Gmail : CarWow@gmail.com</a></li>
+                                    </ul>
+                                </li>
+
+                            </ul>
+                            <form class="d-flex" role="search">
+                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                                <button class="btn btn-outline-success" type="submit">Search</button>
+                            </form>
+                        </div>
+                    </div>
+                </nav>
+                {/* NAVBAR CLOSE */}
+            </header>
+
+            <main>
+                <div class="spinner-border m-5" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
+
+
+            </main>
+
+            <footer>
+
+            </footer>
+        </>
+    )
+}
+
+export default Home;
